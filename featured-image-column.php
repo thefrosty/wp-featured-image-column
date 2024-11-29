f5baf780ee94f193f6205ca107c2aed04436ff7c
Austin Passy <367897+thefrosty@users.noreply.github.com>
2024-11-29 08:55:39 -0800

    Fix/call user func array (#2)
    * Resolve add_action call_user_finc on protected methods.
    
    * Bump version and add activation hook back in.

--- featured-image-column.php
+++ featured-image-column.php
@@ -4,13 +4,13 @@
  * Plugin Name: Featured Image Column
  * Plugin URI: https://austin.passy.co/wordpress-plugins/featured-image-column
  * Description: Adds a column to the edit screen with the featured image if it exists.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Austin Passy
  * Author URI: httsp://austin.passy.co
  * Requires at least: 6.2
- * Tested up to: 6.4.0
+ * Tested up to: 6.7.1
  * Requires PHP: 8.0
- * @copyright 2009 - 2023
+ * @copyright 2009 - 2025
  * @author Austin Passy
  * @link http://frosty.media/
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -22,13 +22,20 @@
  * @package TheFrosty\Featured_Image_Column
  */
 
+use TheFrosty\FeatureImageColumn;
+
 if (!function_exists('add_filter')) {
     header('Status: 403 Forbidden');
     header('HTTP/1.1 403 Forbidden');
     exit;
 }
 
-add_action('init', function (): void {
+add_action('_admin_menu', function (): void {
     require_once __DIR__ . '/src/FeatureImageColumn.php';
-    (new \TheFrosty\FeatureImageColumn())->addHooks();
+    (new FeatureImageColumn(__FILE__))->addHooks();
+});
+
+register_activation_hook(__FILE__, static function (): void {
+    require_once __DIR__ . '/src/FeatureImageColumn.php';
+    (new FeatureImageColumn(__FILE__))->activationHook();
 });
--- src/FeatureImageColumn.php
+++ src/FeatureImageColumn.php
@@ -6,7 +6,7 @@ namespace TheFrosty;
 
 /**
  * Class FeatureImageColumn
- * @package src
+ * @package TheFrosty
  */
 class FeatureImageColumn
 {
@@ -14,15 +14,22 @@ class FeatureImageColumn
     protected const ID = 'featured-image';
 
     /**
+     * FeatureImageColumn constructor.
+     * @param string $file
+     */
+    public function __construct(private string $file)
+    {
+    }
+
+    /**
      * Ensures that the rest of the code only runs on edit.php pages
-     *
      * @since 0.3
      */
     public function addHooks(): void
     {
-        add_action('admin_menu', call_user_func([$this, 'adminMenu']));
-        add_action('admin_init', call_user_func([$this, 'adminInit']));
-        add_action('admin_enqueue_scripts', call_user_func([$this, 'style']));
+        \add_action('admin_menu', [$this, 'adminMenu']);
+        \add_action('admin_init', [$this, 'adminInit']);
+        \add_action('admin_enqueue_scripts', [$this, 'style']);
     }
 
     /**
@@ -32,20 +39,20 @@ class FeatureImageColumn
     {
         $post_types = [];
         foreach ($this->getPostTypes() as $post_type) {
-            if (!post_type_supports($post_type, 'thumbnail')) {
+            if (!\post_type_supports($post_type, 'thumbnail')) {
                 continue;
             }
             $post_types[$post_type] = $post_type;
         }
-        update_option('featured_image_column', $post_types);
+        \update_option('featured_image_column', $post_types);
     }
 
     /**
      * Register our settings page.
      */
-    protected function adminMenu(): void
+    public function adminMenu(): void
     {
-        add_options_page(
+        \add_options_page(
             'Featured Image Column Settings',
             'Featured Image Col',
             'manage_options',
@@ -60,14 +67,14 @@ class FeatureImageColumn
      * Since the load-edit.php hook is too early for checking the post type, hook the rest
      * of the code to the wp action to allow the query to be run first.
      */
-    protected function adminInit(): void
+    public function adminInit(): void
     {
         global $pagenow;
 
         $this->registerSettings();
 
         // Only continue if we're on the 'edit.php' page(s)
-        if (empty($pagenow) || $pagenow !== 'edit.php' && defined('DOING_AJAX') && !DOING_AJAX) {
+        if (empty($pagenow) || $pagenow !== 'edit.php' && \defined('DOING_AJAX') && !\DOING_AJAX) {
             return;
         }
 
@@ -79,49 +86,25 @@ class FeatureImageColumn
 
         // Add out custom column and column data
         foreach ($post_types as $post_type) {
-            if (!post_type_supports($post_type, 'thumbnail')) {
+            if (!\post_type_supports($post_type, 'thumbnail')) {
                 continue;
             }
-            add_filter("manage_{$post_type}_posts_columns", \call_user_func([$this, 'columns']));
-            add_action("manage_{$post_type}_posts_custom_column", \call_user_func([$this, 'columnData']), 10, 2);
+            \add_filter("manage_{$post_type}_posts_columns", [$this, 'columns']);
+            \add_action("manage_{$post_type}_posts_custom_column", [$this, 'columnData'], 10, 2);
         }
-    }
-
-    /**
-     * Output our settings.
-     * @since 0.2.2
-     */
-    protected function settingsPage(): void
-    {
-        $post_types = $this->getSettings();
-        if (empty($post_types)) {
-            $post_types = [];
-            foreach ($this->getPostTypes() as $key => $post_type) {
-                if (post_type_supports($post_type, 'thumbnail')) {
-                    $post_types[$post_type] = $post_type;
-                }
-            }
-            update_option('featured_image_column', $post_types);
-        }
-
-        include dirname(__FILE__, 2) . '/views/settings.php';
     }
 
     /**
      * Enqueue our stylesheet on the edit.php page.
-     * @param string $hook
      */
-    protected function style($hook): void
+    public function style(): void
     {
-        wp_register_style(
-            'featured-image-column',
-            plugin_dir_url(__FILE__) . 'css/column.css',
-            [],
-            '20170625'
-        );
+        global $pagenow;
+        $version = \get_plugin_data($this->file, false, false)['Version'] ?? '20170625';
+        \wp_register_style('featured-image-column', \plugin_dir_url($this->file) . 'css/column.css', [], $version);
 
-        if ($hook === 'edit.php') {
-            wp_enqueue_style('featured-image-column');
+        if ($pagenow === 'edit.php') {
+            \wp_enqueue_style('featured-image-column');
         }
     }
 
@@ -130,13 +113,13 @@ class FeatureImageColumn
      * @param array $columns
      * @return array
      */
-    protected function columns(array $columns): array
+    public function columns(array $columns): array
     {
         $new_columns = [];
         foreach ($columns as $key => $title) {
             // Put the Thumbnail column before the Title column
             if ($key === 'title') {
-                $new_columns[self::ID] = esc_html__('Image', 'featured-image-column');
+                $new_columns[self::ID] = \esc_html__('Image', 'featured-image-column');
             }
 
             $new_columns[$key] = $title;
@@ -151,7 +134,7 @@ class FeatureImageColumn
      * @param string $column_name
      * @param int $post_id
      */
-    protected function columnData(string $column_name, int $post_id):  void
+    public function columnData(string $column_name, int $post_id): void
     {
         if (self::ID !== $column_name) {
             return;
@@ -169,19 +152,39 @@ class FeatureImageColumn
     }
 
     /**
+     * Output our settings.
+     * @since 0.2.2
+     */
+    protected function settingsPage(): void
+    {
+        $post_types = $this->getSettings();
+        if (empty($post_types)) {
+            $post_types = [];
+            foreach ($this->getPostTypes() as $key => $post_type) {
+                if (\post_type_supports($post_type, 'thumbnail')) {
+                    $post_types[$post_type] = $post_type;
+                }
+            }
+            \update_option('featured_image_column', $post_types);
+        }
+
+        include \dirname($this->file) . '/views/settings.php';
+    }
+
+    /**
      * Register the plugins settings.
      */
     protected function registerSettings(): void
     {
-        register_setting(
+        \register_setting(
             'featured_image_column_post_types',
             'featured_image_column',
             static function (mixed $input): array {
-                if (!is_array($input)) {
-                    $input = (array)$input;
+                if (!\is_array($input)) {
+                    $input = (array) $input;
                 }
 
-                return array_map('sanitize_key', $input);
+                return \array_map('sanitize_key', $input);
             }
         );
     }
@@ -193,23 +196,14 @@ class FeatureImageColumn
      */
     protected function getTheImage(?int $post_id): string
     {
-        if (has_post_thumbnail($post_id)) {
-            return get_the_post_thumbnail($post_id);
+        if (\has_post_thumbnail($post_id)) {
+            return \get_the_post_thumbnail($post_id);
         }
 
-        $default = plugins_url('images/default.png', __FILE__);
-        $image = apply_filters('featured_image_column_default_image', $default);
+        $default = \plugins_url('images/default.png', $this->file);
+        $image = \apply_filters('featured_image_column_default_image', $default);
 
-        return sprintf('<img alt="%1$s" src="%2$s">', esc_attr(get_the_title($post_id)), esc_url($image));
-    }
-
-    /**
-     * Get our settings from the DB with the defaults set to an array.
-     * @return array
-     */
-    protected function getSettings(): array
-    {
-        return get_option('featured_image_column', []);
+        return \sprintf('<img alt="%1$s" src="%2$s">', \esc_attr(\get_the_title($post_id)), \esc_url($image));
     }
 
     /**
@@ -218,6 +212,15 @@ class FeatureImageColumn
      */
     protected function getPostTypes(): array
     {
-        return get_post_types(['public' => true]);
+        return \get_post_types(['public' => true]);
+    }
+
+    /**
+     * Get our settings from the DB with the defaults set to an array.
+     * @return array
+     */
+    protected function getSettings(): array
+    {
+        return \get_option('featured_image_column', []);
     }
 }
