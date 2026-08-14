7915edfaa193340c888c83451b658df895f7d5f7
Austin Passy <367897+thefrosty@users.noreply.github.com>
2026-08-14 09:17:30 -0700

    Version 1.3.0.

--- featured-image-column.php
+++ featured-image-column.php
@@ -4,13 +4,13 @@
  * Plugin Name: Featured Image Column
  * Plugin URI: https://austin.passy.co/wordpress-plugins/featured-image-column
  * Description: Adds a column to the edit screen with the featured image if it exists.
- * Version: 1.2.0
+ * Version: 1.3.0
  * Author: Austin Passy
  * Author URI: httsp://austin.passy.co
  * Requires at least: 6.2
  * Tested up to: 7.1.0
  * Requires PHP: 8.0
- * @copyright 2009 - 2025
+ * @copyright 2009 - 2026
  * @author Austin Passy
  * @link http://frosty.media/
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -30,7 +30,7 @@ if (!function_exists('add_filter')) {
     exit;
 }
 
-add_action('_admin_menu', function (): void {
+add_action('_admin_menu', static function (): void {
     require_once __DIR__ . '/src/FeatureImageColumn.php';
     (new FeatureImageColumn(__FILE__))->addHooks();
 });
